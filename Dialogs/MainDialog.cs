41dd9cd0045a8a0ee4f35994f8353e128614e5e0
sergiorafen <rafensergio@gmail.com>
2021-08-09 14:28:51 +0200

    Test modif Bonjour
    Bonjour

--- Dialogs/MainDialog.cs
+++ Dialogs/MainDialog.cs
@@ -51,7 +51,7 @@ namespace Microsoft.BotBuilderSamples.Dialogs
 
             // Use the text provided in FinalStepAsync or the default if it is the first time.
             var weekLaterDate = DateTime.Now.AddDays(7).ToString("MMMM d, yyyy");
-            var messageText = stepContext.Options?.ToString() ?? $"What can I help you with today?\nSay something like \"Book a flight from Paris to Berlin on {weekLaterDate}\"";
+            var messageText = stepContext.Options?.ToString() ?? $"Bonjour";
             var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
         }
